aed3f9dba58d26cb2e78f2e5e0000d30ba31b225
Angelo Montini <angelo.montini@gmail.com>
2025-06-12 09:18:31 +0200

    semi-frerddo

---
+++ script.js
@@ -0,0 +1,18 @@
+// Animazione di rivelazione durante lo scroll
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+document.querySelectorAll(".portata").forEach((el) => {
+  observer.observe(el);
+});
